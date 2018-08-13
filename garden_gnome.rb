a32087b88bcb60ec455f5cb3e69fd79f06e95b90
dami21 <dami.adenekan@schaumburgchristian.com>
2018-08-13 13:53:56 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -14,6 +14,6 @@ class GardenGnome
   end 
   def introduce_self 
     test_gnome1.name = "Carl the Crappy"
-    
+    puts "Hello humans, my name is #{test_gnome1.name} I am 5 years old, and you'll rue the day you crossed me!"
 end 
     
