8cbf2f433e88e524e43703fa4c6ce1122697e109
dami21 <dami.adenekan@schaumburgchristian.com>
2018-08-13 13:49:53 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,13 +1,13 @@
 # Code your instances here
 class GardenGnome
-  # def personality=(gluten_allergy) 
-  #   @personality = evil 
-  # end 
-  # def hat_color=()
-  # end
-  # def gnaw 
-  #   puts "Gnawing on a tree!!!"
-  # end 
+  def personality=(gluten_allergy) 
+    @personality = evil 
+  end 
+  def hat_color=()
+  end
+  def gnaw 
+    puts "Gnawing on a tree!!!"
+  end 
   def shout 
     name = "GNARLY!!!"
     return name 
